fa6fef0659b6548b1ce9d0a39e1b7d5519db6ab6
Jonathan abadie <abadiejoni@hotmail.fr>
2020-12-04 16:32:44 +0100

    FT now checks to-do list's item's creation

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -15,15 +17,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # He checks he's on the right website by looking if to-do lists is in the title
         self.assertIn('To-Do', self.browser.title)
-        self.fail('FINISH TEST')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # He is invited to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # He types "Buy a new laptop" into a text box
+        inputbox.send_keys('Buy a new laptop')
 
         # When he hits enter, the page updates, and now the page lists
         # "1: Buy a new laptop" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == "Buy a new laptop" for row in rows)
+        )
+
+        self.fail('FINISH TEST')
         # There is still a text box inviting him to add another item
 
         # He enters "Configure the new laptop"
