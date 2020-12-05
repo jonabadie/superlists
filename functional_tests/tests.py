9ccb82d2ecd5ef8844fabd9622741d0330bd1def
Jonathan abadie <abadiejoni@hotmail.fr>
2020-12-05 14:32:05 +0100

    Use my own waiter class

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,23 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+from .waiter import JoWaiter
 
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.waiter = JoWaiter(
+            self.browser,
+            timeout=3,
+            ignored_exceptions=[
+                AssertionError,
+                WebDriverException
+            ]
+        )
 
     def tearDown(self):
         self.browser.quit()
@@ -15,7 +26,6 @@ class NewVisitorTest(LiveServerTestCase):
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys(text)
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
     def input_box_placeholder_present(self):
         input_box = self.browser.find_element_by_id('id_new_item')
@@ -30,6 +40,7 @@ class NewVisitorTest(LiveServerTestCase):
         rows_text = [row.text for row in rows]
         for text in texts:
             self.assertIn(text, rows_text)
+        return True
 
     def test_can_start_list_and_retrieve_later(self):
         # User go on the website
@@ -48,7 +59,8 @@ class NewVisitorTest(LiveServerTestCase):
         self.typing_in_list_input('Buy a new laptop')
 
         # And now the page lists "1: Buy a new laptop" as an item in a to-do list
-        self.check_for_row_in_list_table(["1: Buy a new laptop"])
+        self.waiter.until(lambda b: self.check_for_row_in_list_table(["1: Buy a new laptop"]))
+        # self.check_for_row_in_list_table(["1: Buy a new laptop"])
 
         # There is still a text box inviting him to add another item
         self.input_box_placeholder_present()
@@ -57,7 +69,10 @@ class NewVisitorTest(LiveServerTestCase):
         self.typing_in_list_input('Configure the new laptop')
 
         # Page update again and shows both item in the list
-        self.check_for_row_in_list_table(["1: Buy a new laptop", "2: Configure the new laptop"])
+        self.waiter.until(
+            lambda b:
+            self.check_for_row_in_list_table(["1: Buy a new laptop", "2: Configure the new laptop"])
+        )
 
         self.fail('FINISH TEST')
         # User wonder if the website will remember the list
---
+++ functional_tests/waiter.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+import time
+
+
+# Modify WebDriverWait's 'until' function to return the last exception it intercepted
+class JoWaiter(WebDriverWait):
+    def until(self, method, message=''):
+        """Calls the method provided with the driver as an argument until the \
+        return value is not False."""
+        exception = None  # Diff with super
+
+        end_time = time.time() + self._timeout
+        while True:
+            try:
+                value = method(self._driver)
+                if value:
+                    return value
+            except self._ignored_exceptions as exc:
+                exception = exc  # Diff with super
+            time.sleep(self._poll)
+            if time.time() > end_time:
+                break
+        if exception:  # Diff with super
+            raise exception
+        raise TimeoutException(message)
