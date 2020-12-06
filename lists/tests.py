8d8e833996de42e60497309240f80606386a9ef7
Jonathan abadie <abadiejoni@hotmail.fr>
2020-12-06 11:58:37 +0100

    new URL + view for adding item to lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from lists.models import Item, List
 
@@ -21,21 +22,55 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': "A new list item"})
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, '/lists/1/')
+
+
+class NewItemTest(TestCase):
+    def test_can_save_POST_request(self):
+        List.objects.create()
+        list2 = List.objects.create()
+
+        self.client.post(f'/lists/{list2.id}/add_item', data={'item_text': "A new list item"})
+
+        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(Item.objects.first().text, "A new list item")
+        self.assertEqual(Item.objects.first().list, list2)
+
+    def test_redirects_after_POST(self):
+        List.objects.create()
+        list_ = List.objects.create()
+
+        response = self.client.post(f'/lists/{list_.id}/add_item', data={'item_text': "A new list item"})
+        self.assertRedirects(response, f'/lists/{list_.id}/')
 
 
 class ListViewTest(TestCase):
+    def test_use_list_temple(self):
+        list_ = List.objects.create()
+        response = self.client.get(reverse('view-list', args=[list_.id]))
+        self.assertTemplateUsed(response, 'lists/list.html')
+
+    def test_passes_correct_list_to_template(self):
+        List.objects.create()
+        list_ = List.objects.create()
+        response = self.client.get(reverse('view-list', args=[list_.id]))
+        self.assertEqual(response.context['list'], list_)
+
     def test_items_shows_in_page(self):
-        texts = ["First item", "Second item"]
+        texts = ["First item", "Second item", "Third item", "Fourth item"]
         list_ = List.objects.create()
         Item.objects.create(text=texts[0], list=list_)
         Item.objects.create(text=texts[1], list=list_)
+        list2 = List.objects.create()
+        Item.objects.create(text=texts[2], list=list2)
+        Item.objects.create(text=texts[3], list=list2)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        response = self.client.get(reverse('view-list', args=[list_.id]))
 
         self.assertContains(response, texts[0])
         self.assertContains(response, texts[1])
-        self.assertTemplateUsed(response, 'lists/list.html')
+        self.assertNotContains(response, texts[2])
+        self.assertNotContains(response, texts[3])
 
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Item, List
 
@@ -7,13 +7,22 @@ def home_page(request):
     return render(request, 'lists/home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'lists/list.html', {'items': items})
+def view_list(request, list_id):
+    list_ = get_object_or_404(List, pk=list_id)
+    return render(request, 'lists/list.html', {'list': list_})
 
 
 def new_list(request):
     text = request.POST.get('item_text')
     if text:
-        Item.objects.create(text=text, list=List.objects.create())
-    return redirect('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()
+        Item.objects.create(text=text, list=list_)
+        return redirect(f'/lists/{list_.id}/')
+
+
+def new_item(request, list_id):
+    text = request.POST.get('item_text')
+    list_ = get_object_or_404(List, pk=list_id)
+    if text:
+        Item.objects.create(text=text, list=list_)
+        return redirect(f'/lists/{list_.id}/')
