7442261e07b02ae6b5241388f5c28c37f66deba2
Jonathan abadie <abadiejoni@hotmail.fr>
2020-12-06 15:18:00 +0100

    Start layout test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -58,7 +58,6 @@ class NewVisitorTest(LiveServerTestCase):
         # When he hits enter, the page updates
         self.typing_in_list_input('Buy a new laptop')
 
-        time.sleep(1)
         # And now the page lists "1: Buy a new laptop" as an item in a to-do list
         self.waiter.until(lambda b: self.check_for_row_in_list_table(["1: Buy a new laptop"]))
 
@@ -109,3 +108,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(user_list_url, jacob_list_url)
 
         # Satisfied
+
+    def test_layout_and_styling(self):
+        # Jacob goes on the website on a small computer screen
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Jacob notice that the input box is nicely centered
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # He types a new list and sees the input is nicely centered there too
+        self.typing_in_list_input('Buy a new laptop')
+        self.waiter.until(lambda b: self.check_for_row_in_list_table(["1: Buy a new laptop"]))
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
